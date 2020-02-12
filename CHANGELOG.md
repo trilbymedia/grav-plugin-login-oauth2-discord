1bc93434681768dbf6e09ffdb3e5db0ccd3c764a
Djamil Legato <djamil@djamil.it>
2020-02-12 14:06:22 -0800

    Fixed plugin to reflect BaseProvider changes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
+# v1.0.1
+## 02/12/2020
+
+1. [](#bugfix)
+    * Fixed plugin to reflect BaseProvider changes
+
+
 # v1.0.0
 ##  04/21/2018
 
--- classes/Providers/DiscordProvider.php
+++ classes/Providers/DiscordProvider.php
@@ -12,7 +12,7 @@ class DiscordProvider extends BaseProvider
     /** @var Discord */
     protected $provider;
 
-    public function __construct(array $options)
+    public function initProvider(array $options)
     {
         $this->config = Grav::instance()['config'];
 
@@ -22,7 +22,7 @@ class DiscordProvider extends BaseProvider
             'redirectUri'   => $this->getCallbackUri(),
         ];
 
-        parent::__construct($options);
+        parent::initProvider($options);
     }
 
     public function getAuthorizationUrl()
